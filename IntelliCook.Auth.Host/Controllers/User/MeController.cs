7856c721f18bea96ecc5b5e58e3c6d765efde4d3
LioQing <s2010157@gmail.com>
2024-08-13 00:19:10 +0800

    Add delete method for user me endpoint

--- IntelliCook.Auth.Host/Controllers/User/MeController.cs
+++ IntelliCook.Auth.Host/Controllers/User/MeController.cs
@@ -21,27 +21,12 @@ public class MeController(UserManager<IntelliCookUser> userManager) : Controller
     [ProducesResponseType(typeof(UserGetResponseModel), StatusCodes.Status200OK)]
     [ProducesResponseType(typeof(ValidationProblemDetails), StatusCodes.Status400BadRequest)]
     [ProducesResponseType(typeof(void), StatusCodes.Status401Unauthorized)]
+    [ProducesResponseType(typeof(ProblemDetails), StatusCodes.Status404NotFound)]
     public async Task<IActionResult> Get()
     {
-        var name = User.Claims
-            .SingleOrDefault(c => c.Type == ClaimTypes.Name)?
-            .Value;
+        var name = GetUsername();
 
         if (name == null)
-        {
-            ModelState.AddModelError(nameof(ClaimTypes.Name), "Invalid token with no name is given.");
-        }
-
-        var role = User.Claims
-            .SingleOrDefault(c => c.Type == ClaimTypes.Role)?
-            .Value;
-
-        if (role == null)
-        {
-            ModelState.AddModelError(nameof(ClaimTypes.Role), "Invalid token with no role is given.");
-        }
-
-        if (name == null || role == null)
         {
             return BadRequest(this.CreateValidationProblemDetails());
         }
@@ -64,5 +49,76 @@ public class MeController(UserManager<IntelliCookUser> userManager) : Controller
             "User not found",
             detail: "Invalid token with no user found."
         ));
+    }
+
+    /// <summary>
+    ///     Deletes the current user.
+    /// </summary>
+    [HttpDelete]
+    [ProducesResponseType(StatusCodes.Status204NoContent)]
+    [ProducesResponseType(typeof(ValidationProblemDetails), StatusCodes.Status400BadRequest)]
+    [ProducesResponseType(typeof(void), StatusCodes.Status401Unauthorized)]
+    [ProducesResponseType(typeof(ProblemDetails), StatusCodes.Status404NotFound)]
+    public async Task<IActionResult> Delete()
+    {
+        var name = GetUsername();
+
+        if (name == null)
+        {
+            return BadRequest(this.CreateValidationProblemDetails());
+        }
+
+        var user = await userManager.FindByNameAsync(name);
+
+        if (user == null)
+        {
+            return NotFound(this.CreateProblemDetails(
+                StatusCodes.Status404NotFound,
+                "User not found",
+                detail: "Invalid token with no user found."
+            ));
+        }
+
+        var result = await userManager.DeleteAsync(user);
+
+        if (result.Succeeded)
+        {
+            return NoContent();
+        }
+
+        foreach (var error in result.Errors)
+        {
+            ModelState.AddModelError(string.Empty, error.Description);
+        }
+
+        return BadRequest(this.CreateValidationProblemDetails());
+    }
+
+    private string? GetUsername()
+    {
+        var name = User.Claims
+            .SingleOrDefault(c => c.Type == ClaimTypes.Name)?
+            .Value;
+
+        if (name == null)
+        {
+            ModelState.AddModelError(nameof(ClaimTypes.Name), "Invalid token with no name is given.");
+        }
+
+        var role = User.Claims
+            .SingleOrDefault(c => c.Type == ClaimTypes.Role)?
+            .Value;
+
+        if (role == null)
+        {
+            ModelState.AddModelError(nameof(ClaimTypes.Role), "Invalid token with no role is given.");
+        }
+
+        if (name == null || role == null)
+        {
+            return null;
+        }
+
+        return name;
     }
 }
