2c2900b7a92372d836e70ca4e958bd0048b5461a
LioQing <s2010157@gmail.com>
2024-08-31 15:38:11 +0800

    Add user put endpoints unit tests

--- IntelliCook.Auth.Contract/User/UserPasswordPutRequestModel.cs
+++ IntelliCook.Auth.Contract/User/UserPasswordPutRequestModel.cs
@@ -6,5 +6,11 @@ public class UserPasswordPutRequestModel
 {
     [Required] public string OldPassword { get; set; }
 
-    [Required] public string NewPassword { get; set; }
+    [Required]
+    [MinLength(8)]
+    [RegularExpression(
+        @"^(?=.*[a-z]{1,})(?=.*[A-Z]{1,})(?=.*[0-9]{1,}).+$",
+        ErrorMessage = "Password must contain at least one lowercase letter, one uppercase letter, and one digit."
+    )]
+    public string NewPassword { get; set; }
 }
--- IntelliCook.Auth.Contract/User/UserPutRequestModel.cs
+++ IntelliCook.Auth.Contract/User/UserPutRequestModel.cs
@@ -4,9 +4,24 @@ namespace IntelliCook.Auth.Contract.User;
 
 public class UserPutRequestModel
 {
-    [Required] public string Name { get; set; }
+    [Required]
+    [MaxLength(256)]
+    [RegularExpression(
+        @"^.*\S.*$",
+        ErrorMessage = "Name cannot contain only whitespace characters."
+    )]
+    public string Name { get; set; }
 
-    [Required] public string Username { get; set; }
+    [Required]
+    [MaxLength(256)]
+    [RegularExpression(
+        @"^[0-9a-zA-Z-\._@\+]+$",
+        ErrorMessage = "Username can only contain letters, digits, '-', '.', '_', '@', and '+'."
+    )]
+    public string Username { get; set; }
 
-    [Required] public string Email { get; set; }
+    [Required]
+    [MaxLength(256)]
+    [EmailAddress]
+    public string Email { get; set; }
 }
